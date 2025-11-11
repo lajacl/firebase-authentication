5fe1d35692b26a0751984e73600975b2190a5394
lajacl <lavette.clifton@gmail.com>
2025-11-10 20:47:49 -0500

    Create auth service as wrapper for firebase auth

---
+++ lib/auth_service.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // Stream to listen to authentication state changes
+  Stream<User?> get user {
+    return _firebaseAuth.authStateChanges();
+  }
+
+  // Register with email and password
+  Future<User?> registerWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      UserCredential result = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return result.user;
+    } on FirebaseAuthException catch (e) {
+      // Handle specific Firebase authentication errors
+      debugPrint(e.message);
+      return null;
+    } catch (e) {
+      debugPrint(e.toString());
+      return null;
+    }
+  }
+
+  // Sign in with email and password
+  Future<User?> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      UserCredential result = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    } on FirebaseAuthException catch (e) {
+      // Handle specific Firebase authentication errors
+      debugPrint(e.message);
+      return null;
+    } catch (e) {
+      debugPrint(e.toString());
+      return null;
+    }
+  }
+
+  // Sign out
+  Future<void> signOut() async {
+    try {
+      return await _firebaseAuth.signOut();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+  // Get current user
+  User? getCurrentUser() {
+    return _firebaseAuth.currentUser;
+  }
+}
